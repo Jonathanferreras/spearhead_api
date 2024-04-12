3d999e615a41a4869c093ba0abdca63ce4a48d8c
jonathanferreras <jonathanferreras@icloud.com>
2024-04-12 10:29:30 -0400

    updated yaml files

--- spearhead_api/settings.py
+++ spearhead_api/settings.py
@@ -74,10 +74,6 @@ WSGI_APPLICATION = 'spearhead_api.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
